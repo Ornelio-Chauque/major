268e6f2904aded2b8cb87647b697f7544c5434c7
Ornelio Chauque <ornelio.chauque@vm.co.mz>
2023-04-28 08:18:08 +0200

    fix: update the argument list passed to run method of major

--- internal/major.go
+++ internal/major.go
@@ -4,19 +4,16 @@ import (
 	"fmt"
 	"os/exec"
 	//"syscall"
-
 )
-
 
 func Run(args []string) {
 
 	/*we expect at least two argument when run the application, the application binary and one or more argument
 	Ex: ./major cat myfile.txt
 	*/
-	if len(args)< 2{
+	if len(args) < 2 {
 		panic("Not enough argument provided, -h for help")
 	}
-
 
 	/* To be able to work with file with root previlegy we must set the effective user to previleed user (root)
 	   The Seteuid becomes inrrelevant when the application file owner uid passed to exec syscall is set to root
@@ -27,14 +24,13 @@ func Run(args []string) {
 	// }
 
 	//setup the command to executed and it arguments
-	cmd := exec.Command(args[1], args[2:]...);
+	cmd := exec.Command(args[0], args[1:]...)
 
 	//Run the binary/command wait it finish an then get the content sent to stdout by the binary in the exec syscall
-	out, errr:= cmd.Output();
+	out, errr := cmd.Output()
 
-
-	if errr != nil{
-		switch e:= errr.(type){
+	if errr != nil {
+		switch e := errr.(type) {
 
 		case *exec.ExitError:
 			panic(string(e.Stderr))
